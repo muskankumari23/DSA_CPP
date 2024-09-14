754ffe187c74ab26adefca12eb2e5d66bf96f2f7
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-14 21:15:42 +0530

    feel better

---
+++ DAY-7/Pattern ch.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+ int main (){
+    int n=4;
+    for (int i=0; i<n; i++){
+    char ch='a';
+    for (int j=0; j<n; j++){
+        cout<<ch;
+        ch=ch+1
+    } cout<< endl;
+        }
+        return 0;
+    }
+ 
---
+++ DAY-7/Pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ int main(){
+    int n=4;
+    for(int i=1; i<=n; i++){
+    for (int j=; j<=n; j++){
+        cout<<j;
+    }
+    cout<<endl;
+ }
+ return 0;
+ }

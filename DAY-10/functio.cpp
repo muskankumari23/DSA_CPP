2222c368bb67051f306db407912f6e334329315e
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-20 21:46:43 +0530

    .

---
+++ DAY-10/functio.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    void PrintHello(){
+        cout <<"hello\n";
+    }
+    int main(){
+        printHello();
+        return 0;
+    }
+ }
---
+++ DAY-10/functioSum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main (){
+    int sum (int a int b){
+        int s=a+b 
+        return s;
+
+    }
+    int main(){
+        cout <<sum (10,5)<<endl;
+        return 0;
+    }
+ }

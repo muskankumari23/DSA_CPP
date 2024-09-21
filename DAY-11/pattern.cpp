73e1a7e88bf6bcd280c51da519486ce15a501ac8
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-21 11:03:16 +0530

    .

---
+++ DAY-11/pattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+ int min (){
+    int n=5;
+     
+    for(i=0; i<n; i++){
+        char ch='A';
+        for (j=0; j<n; j++){
+            cout <<ch;
+            ch=ch+1;
+        }
+        cout<<endl;
+    }
+
+ }
--- DAY-7/pattren4.cpp
+++ DAY-7/pattren4.cpp
@@ -2,8 +2,8 @@
 using namespace std;
  int main(){
     int n=4;
-    for (int i=0; i<+n; i++){
-        for(int j=0; j<=n; j++){
+    for (int i=0; i<=n; i++){
+        for(int j=0; j-n-1; j++){
             cout<<j;
             {
                 cout<<endl;

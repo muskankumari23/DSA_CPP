c25c295d0040e8998c88c6f415d242381781a117
MuskanKumari23 <muskankumari92084@Gmail.com>
2024-09-25 21:59:13 +0530

    .

---
+++ DAY-11/Array2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main(){ 
+    int num []={20, 30,40, 50, 60, };
+    int size =5;
+ int smallest= +70;
+ for (i=0; i<size; i++){
+    smallest=min (num[i], smallest);
+    cout<<"smallest=" <<smallest <<endl;
+    return 0;
+
+ }
+ }

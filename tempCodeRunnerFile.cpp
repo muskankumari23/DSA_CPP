cf16c91dee249ff67d7a55145659954ace008fbe
MuskanKumari23 <muskankumari92084@Gmail.com>
2025-04-11 01:26:07 +0530

    .

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostrem> 
+using namespace std;
+
+int main(){
+   String name;
+   cout<<"Enter your name = ";
+
+   cin>> name;
+
+   cout<<"Your name is =";
+   
+     
+    return 0;
+}
